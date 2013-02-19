98d084d1d4543ad6ae9164cc8fbca9b6503108f0
rnav529 <rnrvdz@gmail.com>
2013-02-19 13:23:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galley::Application.config.secret_token = '471a097bed458e1ddf4536dc6bad84a5fe5c679857cacce26d7e867866b85886903a6741b5afdc1da1b2aa0ccce35f638f18e6262dd986b3ec955be964cfc63e'
